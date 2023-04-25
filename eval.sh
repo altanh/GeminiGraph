cb1ce6c9820a0a4d289a80f83467e66bef76db41
manishshettym <manishs@berkeley.edu>
2023-04-25 09:33:00 -0700

    running gemini with craypat

---
+++ eval.sh
@@ -0,0 +1,30 @@
+# run module load perftools-base
+# run module load perftools-lite
+# use CC as the compiler
+
+# run salloc -N 2 -C cpu -q interactive -t 01:00:00 before running this script
+
+make -j 40
+
+logs_dir="logs"
+mkdir $logs_dir
+
+export PAT_RT_MPI_THREAD_REQUIRED=3
+
+echo "CC with P = [1 2 4 8 16 32 64]"
+for P in 1 2 4 8 16 32 64
+do
+    srun -N 1 --ntasks-per-node=$P ./toolkits/cc ./data/rand_cc_5.mtx.bin64 0 0 >> "$logs_dir/cc_$P.txt"
+done
+
+# echo "Betweenness Centrality"
+# ./toolkits/bc ./data/rand_cc_5.mtx.bin64 0 0 0
+
+# echo "Breadth First Search"
+# ./toolkits/bfs ./data/rand_cc_5.mtx.bin64 0 0 0
+
+# echo "PageRank"
+# ./toolkits/pagerank ./data/rand_cc_5.mtx.bin64 0 0 0
+
+# echo "Single Source Shortest Path"
+# ./toolkits/sssp ./data/rand_cc_5.mtx.bin64 0 0 0
--- run.sh
+++
@@ -1,7 +0,0 @@
-make -j 40
-
-./toolkits/cc ./data/rand_cc_5.mtx.bin64 0 0
-./toolkits/bc ./data/rand_cc_5.mtx.bin64 0 0 0
-./toolkits/bfs ./data/rand_cc_5.mtx.bin64 0 0 0
-./toolkits/pagerank ./data/rand_cc_5.mtx.bin64 0 0 0
-./toolkits/sssp ./data/rand_cc_5.mtx.bin64 0 0 0
